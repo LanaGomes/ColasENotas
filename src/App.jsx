10f1af5b6d2e66ff5f1ca562aab5eb41a4c7585e
Lana Gomes <lana.gomes1919@gmail.com>
2024-01-12 17:22:29 -0300

    overflow content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import Combinators from "./NotasEColas/CSS/Combinators";
-
+import OverflowingContent from "./NotasEColas/CSS/OverflowingContent";
 import PseudoClasses from "./NotasEColas/CSS/PseudoClasses";
 
 function App() {
   return (
     <>
-      <Combinators />
+      <OverflowingContent />
     </>
   );
 }
---
+++ src/NotasEColas/CSS/OverflowingContent.jsx
@@ -0,0 +1,12 @@
+function OverflowingContent() {
+  return (
+    <div>
+      <h2>Overflow solutions</h2>
+      <h3>Overflow property</h3>- visible and not visible (Hide overflowing
+      information) - scroll : Add scrollbars to the content ( mesmo que o
+      conteúdo não esteja ultrapassando). Auto vai fazer as scrollbars
+      aparecerem somente se necessário.
+    </div>
+  );
+}
+export default OverflowingContent;
